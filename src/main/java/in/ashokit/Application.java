31e367077aef6b02c9f57310ffbc4609223eb7af
Manish <158277489+manish29121995@users.noreply.github.com>
2024-03-24 13:02:04 +0530

    Update Application.java

--- src/main/java/in/ashokit/Application.java
+++ src/main/java/in/ashokit/Application.java
@@ -6,6 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class Application {
 	int a = 10;
+	String manish = "HY Manish...";
 
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
